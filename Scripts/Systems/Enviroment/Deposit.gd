847fa2a42848260904054de61c69d5cfdeb171ca
Adrián Chao Béjar <163284713+achabej@users.noreply.github.com>
2025-05-28 13:08:12 +0200

    Entrega11

--- Scripts/Systems/Enviroment/Deposit.gd
+++ Scripts/Systems/Enviroment/Deposit.gd
@@ -2,3 +2,7 @@ extends Node
 
 @export var material: PackedScene = preload("res://Prefabs/Buildings/Materials/Iron.tscn")
 @export var resource_type: String = ""
+
+func _on_screen_exited():
+	set_process(false)
+	set_physics_process(false)
